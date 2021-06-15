30f96eae913de81e3ea9f161d9da71e703a9592d
MeaganRitchie <meaganritchie@gmail.com>
2021-06-14 22:24:05 -0600

    Changes token info.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :crafts
 
   def token
-    secret = Rails.application.secrets.secret_key_base[0]
+    secret = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base[0]
     payload = {user_id: self.id}
     JWT.encode(payload, secret)
   end
